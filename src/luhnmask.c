5b5e32e1dbf7bb55e83d2dcc64e6cd0eb69882c7
John Lombardo <john@lombardos.org>
2013-12-22 20:33:03 -0800

    Hello World becomes cat

--- src/luhnmask.c
+++ src/luhnmask.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+#define MAX_LINE_LENGTH 1024
+
 int main(int argc, char** argv){
-    printf("Hello World\n");
+    char line[MAX_LINE_LENGTH];
+
+    while( fgets(line, MAX_LINE_LENGTH, stdin) != NULL ){
+        fputs(line, stdout);
+    }
 }
